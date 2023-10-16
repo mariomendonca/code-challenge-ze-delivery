529ade5e704c94daafb3c9a1581439978101e973
Mario Mendonca <mariomeendonca00@gmail.com>
2023-10-16 18:06:04 -0300

    remove not used imports

--- src/main/java/com/code_challenge/ze_delivery_code_challenge/controllerAdvice/ExceptionHandlerController.java
+++ src/main/java/com/code_challenge/ze_delivery_code_challenge/controllerAdvice/ExceptionHandlerController.java
@@ -1,12 +1,9 @@
 package com.code_challenge.ze_delivery_code_challenge.controllerAdvice;
 
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
