881eef63459ceeff8e88be4d814b6d833b603644
Mario Mendonca <mariomeendonca00@gmail.com>
2023-10-19 18:09:18 -0300

    feat: create exception to NoSuchElementException

--- src/main/java/com/code_challenge/ze_delivery_code_challenge/controllerAdvice/ExceptionHandlerController.java
+++ src/main/java/com/code_challenge/ze_delivery_code_challenge/controllerAdvice/ExceptionHandlerController.java
@@ -1,9 +1,12 @@
 package com.code_challenge.ze_delivery_code_challenge.controllerAdvice;
 
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
@@ -11,7 +14,10 @@ public class ExceptionHandlerController {
     public ResponseEntity<String> handleDuplicateKeyException(DuplicateKeyException e) {
         return ResponseEntity.badRequest().body(e.getMostSpecificCause().getMessage());
     }
-
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handlerNoSuchElementException(NoSuchElementException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
         return ResponseEntity.badRequest().body(e.getMessage());
